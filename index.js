2026d935d1f6c6f0409e3b7170c920460ed3e7ba
kn <katsuya.noguchi@gmail.com>
2022-09-28 14:03:37 -0400

    Fix dsm issue

--- index.js
+++ index.js
@@ -4,33 +4,42 @@ import { AlchemyProvider } from '@ethersproject/providers'
 import { publishCast } from '@standard-crypto/farcaster-js'
 import { createClient } from '@supabase/supabase-js'
 
+/*
+ * Constants
+ */
 const SUPABASE_URL = 'https://kpwbglpxjuhiqtgtvenz.supabase.co'
 const READ_ONLY_SUPABASE_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwd2JnbHB4anVoaXF0Z3R2ZW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgzNzg2MjEsImV4cCI6MTk3Mzk1NDYyMX0.zecokpSRK0MI_nOaSAgFZJCMkPSpEXraPKqQD5fogE4'
-const supabase = createClient(
-  SUPABASE_URL,
-  process.env.SUPABASE_KEY || READ_ONLY_SUPABASE_KEY
-)
+const supabase = createClient(SUPABASE_URL, READ_ONLY_SUPABASE_KEY)
+
 const NCBOT_ADDRESS = '0x4E9c142eE16e45FFB32Ece493b8e00E8Eeb47260'
 const RECAST_PREFIX = 'recast:farcaster://casts/'
 
 // We recast new users for 3 days.
-const RECAST_FOR_HR = 72
+const RECAST_FOR_USER_HR = 72
 
-// Prevent recasting many posts when ncbot start
-const NCBOT_START_TIME = 1664329238684
+// We recast new casts posted up to 1 hour ago.
+const RECAST_FOR_CAST_HR = 1
 
-const getEntryCreatedAtThreshold = () => {
+/*
+ * Helper functions
+ */
+const getTimeAgo = (hours) => {
   const now = new Date().getTime()
-  return Math.floor(
-    new Date(now - RECAST_FOR_HR * 60 * 60 * 1000).getTime() / 1000
-  )
+  return new Date(now - hours * 60 * 60 * 1000).getTime()
+}
+
+/*
+ * Supabase helper functions
+ */
+const getEntryCreatedAtThreshold = () => {
+  return Math.floor(getTimeAgo(RECAST_FOR_USER_HR) / 1000)
 }
 
 const getNewUsers = async () => {
   const { data, error } = await supabase
     .from('account_view')
-    .select('id, address')
+    .select('username, address')
     .gt('entry_created_at', getEntryCreatedAtThreshold())
 
   if (error) {
@@ -41,6 +50,9 @@ const getNewUsers = async () => {
   return data
 }
 
+/*
+ * Farcaster API helper functions
+ */
 const fetchWithLog = async (url) => {
   try {
     const res = await fetch(url)
@@ -61,14 +73,9 @@ const getCasts = async (address) => {
   )
 }
 
-const getIndexThreshold = () => {
-  const now = new Date().getTime()
-  return new Date(now - RECAST_FOR_HR * 2 * 60 * 60 * 1000).getTime()
-}
-
-const getLatestSequenceRecasted = async () => {
-  const index = {}
-  const publishedThreshold = getIndexThreshold()
+const getLatestSequenceRecastedPerAddress = async () => {
+  const latestSequences = {}
+  const publishedThreshold = getTimeAgo(RECAST_FOR_USER_HR)
   let casts = await getCasts(NCBOT_ADDRESS)
   let passedThreshold = false
   do {
@@ -79,8 +86,11 @@ const getLatestSequenceRecasted = async () => {
         passedThreshold = true
         break
       }
-      if (!index[body.address] || index[body.address] < body.sequence) {
-        index[body.address] = body.sequence
+      if (
+        !latestSequences[body.address] ||
+        latestSequences[body.address] < body.sequence
+      ) {
+        latestSequences[body.address] = body.sequence
       }
     }
     if (passedThreshold) {
@@ -92,13 +102,16 @@ const getLatestSequenceRecasted = async () => {
       break
     }
   } while (true)
-  return index
+  return latestSequences
 }
 
+/*
+ * Main function
+ */
 const recastNewUsers = async () => {
   const users = await getNewUsers()
   console.log(
-    `There are ${users.length} new users created in the past ${RECAST_FOR_HR} hours.`
+    `There are ${users.length} new users created in the past ${RECAST_FOR_USER_HR} hours.`
   )
 
   if (!users.length) return
@@ -108,32 +121,43 @@ const recastNewUsers = async () => {
     ? Wallet.fromMnemonic(process.env.FARCASTER_SEED_PHRASE)
     : ''
 
-  const latestSequenceRecasted = getLatestSequenceRecasted()
+  const latestSequences = getLatestSequenceRecastedPerAddress()
   console.log(
     `Found ${
-      Object.keys(latestSequenceRecasted).length
-    } users recasted in the past ${RECAST_FOR_HR * 2} hours.`
+      Object.keys(latestSequences).length
+    } users @ncbot recasted in the past ${RECAST_FOR_USER_HR} hours.`
   )
 
   for (const user of users) {
+    if (user.username.startsWith('__tt__')) {
+      continue // Skip test users
+    }
     const casts = await getCasts(user.address)
+    const publishedAtThreshold = getTimeAgo(RECAST_FOR_CAST_HR)
     for (const cast of casts.result.casts.reverse()) {
       const { body } = cast
-      const { address, sequence } = body
+      const { address, data, publishedAt, sequence, username } = body
       if (user.address.toLowerCase() !== address.toLowerCase()) {
-        continue // Skip recasts
+        continue // Skip because recasts
       }
-      if (body.publishedAt < NCBOT_START_TIME) {
-        continue
+      if (publishedAt < publishedAtThreshold) {
+        continue // Skip if casted before the threshold to prevent recasting too many
+      }
+      if (data.replyParentMerkleRoot) {
+        continue // Skip replies
       }
       if (
-        latestSequenceRecasted[address] === undefined ||
-        sequence > latestSequenceRecasted[address]
+        latestSequences[address] !== undefined &&
+        sequence <= latestSequences[address]
       ) {
-        wallet &&
-          (await publishCast(wallet, provider, RECAST_PREFIX + cast.merkleRoot))
-        console.log(`Recasted @${body.username}: ${body.data.text}`)
+        continue // Skip if already recasted by ncbot
       }
+      if (data.text.startsWith('Authenticating my Farcaster account')) {
+        continue // Skip auth casts
+      }
+      wallet &&
+        (await publishCast(wallet, provider, RECAST_PREFIX + cast.merkleRoot))
+      console.log(`Recasted @${username}: ${data.text}`)
     }
   }
 
